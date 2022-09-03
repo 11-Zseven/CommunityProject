71f0ba2e167f8e1a1bada23396a4e8738b78e467
root <root@qq.com>
2022-09-03 14:34:00 +0800

    first commit

---
+++ src/test/java/com/lin/communityproject/CommunityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.communityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
